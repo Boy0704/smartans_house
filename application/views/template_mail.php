6b7f831c261b9e852e4ee4107c9aca88ea0c9092
Boy Kurniawan <boykurniawan123@gmail.com>
2020-04-25 11:52:34 +0700

    email template

--- application/views/template_mail.php
+++ application/views/template_mail.php
@@ -6,7 +6,7 @@
 $data = $this->db->get_where('smartans_tagihan_header', array('no_invoice'=>$inv))->row();
 
  ?>
-<body style="background-color:#e2e1e0;font-family: Open Sans, sans-serif;font-size:100%;font-weight:400;line-height:1.4;color:#000;">
+<body style="background-color:#ffffff;font-family: Open Sans, sans-serif;font-size:100%;font-weight:400;line-height:1.4;color:#000;">
   <table style="max-width:670px;margin:50px auto 10px;background-color:#fff;padding:50px;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;-webkit-box-shadow:0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24);-moz-box-shadow:0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24);box-shadow:0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24); border-top: solid 10px green;">
     <thead>
       <tr>
